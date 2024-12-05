dd9cd15b9831003b7e8551460108c8a74c8877c2
Name <fkADGOJWEN@mail.ru>
2024-12-05 19:03:35 +0300

    добавлен файл Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace project3;
+
+class Point
+{
+    int x;
+    int y;
+    char sym;
+
+    public Point(int x, int y, char sym)
+    {
+        this.x = x;
+        this.y = y;
+        this.sym = sym;
+    }
+}
+
+
